3d9226adea7a9ddfe3e74a04f1d7ef78a9ae38b1
Patri <paqueco12@gmail.com>
2026-07-14 07:17:14 +0200

    refactor: aplicar feedback de diseño (sin margin, areas click expandidas, menú hamburguesa empieza en 1024px)

---
+++ src/app/components/image-cropper-modal/image-cropper-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-cropper-modal',
+  imports: [],
+  templateUrl: './image-cropper-modal.html',
+  styleUrl: './image-cropper-modal.scss',
+})
+export class ImageCropperModal {
+
+}
